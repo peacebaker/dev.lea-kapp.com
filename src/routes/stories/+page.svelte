07adc1be6a5687c41a303129c052d9c0b9c00819
peacebaker <116932680+peacebaker@users.noreply.github.com>
2023-08-30 12:20:04 -0400

    begin stories page

---
+++ src/routes/stories/+page.svelte
@@ -0,0 +1,42 @@
+<div class="top">
+  <h1>Stories</h1>
+  <div class="search">Search: <input /></div>
+</div>
+
+<div class="tags">
+  <div class="tag-label">
+    Tags:
+  </div>
+</div>
+
+<style>
+  .top {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    font-family: "Poiret One";
+  }
+  .tags {
+    display: flex;
+    flex-direction: row;
+  }
+  .tag-label {
+    font-family: "Poiret One";
+    font-size: 36px;
+    margin: 0 0 0 16px;
+  }
+  .search {
+    font-size: 36px;
+    margin: 16px;
+  }
+  h1 {
+    font-size: 96px;
+    margin: 16px;
+  }
+  input {
+    border: 0;
+    background-color: inherit;
+    border-bottom: 2px solid var(--primary);
+    color: var(--whitish);
+  }
+</style>
