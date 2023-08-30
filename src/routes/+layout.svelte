736d34a4a892da234e374844be3154aa16207b8d
peacebaker <116932680+peacebaker@users.noreply.github.com>
2023-08-30 12:05:07 -0400

    normalize; apply color scheme; set body defaults

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import "normalize.css"
+  import "@fontsource/poiret-one";
+  import "@fontsource/open-sans";
+</script>
+
+<slot />
+
+<style>
+  :root {
+    --blackish: #24243c;
+    --grayish: #3c3c55;
+    --whitish: #e6e6ff;
+    --primary: #a719ff;
+    --secondary: #ff19ff;
+  }
+
+  :global(body) {
+    margin: 0;
+    background-color: var(--blackish);
+    color: var(--whitish);
+    font-family: "Open Sans";
+  }
+</style>
