6228799082a8050ec2436fbaf06cf00b2ebc8b3f
peacebaker <116932680+peacebaker@users.noreply.github.com>
2023-09-12 22:00:05 -0400

    it is called the backend, after all

---
+++ src/routes/butt/+page.server.js
@@ -0,0 +1,86 @@
+import { MongoClient } from "mongodb";
+import { verify } from "argon2";
+import { redirect } from "@sveltejs/kit";
+
+const uri = "mongodb://localhost:27017";
+
+export async function load({cookies}) {
+
+  // connect to local MongoDB instance
+  const client = new MongoClient(uri);
+  const username = cookies.get('username');
+  const token = cookies.get('token');
+
+  // need to login if no cookies exist
+  if (!username || !token) {
+    return;
+  }
+
+  try {
+
+    // try to find the session in Mongo
+    const db = client.db("leaKapp");
+    const sessions = db.collection('sessions');
+    const query = { user: username, token: token }
+    const session = await sessions.findOne(query);
+
+    // if no session is found, display the login page
+    // TODO: implement some kinda session timeout
+    if (!session) {
+      return;
+
+    // if the user is already logged in, take them to stories
+    } else {
+      throw redirect(307, "/butt/stories")
+    }
+
+  } finally {
+    await client.close()
+  }
+}
+
+export const actions = {
+  default: async ({cookies, request}) => {
+
+    // connect to local MongoDB instance
+    const client = new MongoClient(uri);
+
+    // wait for form submission
+    const data = await request.formData();
+    const username = data.get('username');
+    const password = data.get('password');
+
+    try {
+
+      // find our user
+      const db = client.db("leaKapp");
+      const users = db.collection('users');
+      const query = { user: username };
+      const user = await users.findOne(query);
+
+      // verify the password
+      const result = await verify(user.hash, password)
+      if (result) {
+
+        // generate the session id and stash it in our db
+        const token = crypto.randomUUID()
+        const sessions = db.collection('sessions');
+        await sessions.insertOne({ user: username, token: token})
+
+        // set cookies and redirect
+        cookies.set('username', username, {path: '/', sameSite: true})
+        cookies.set('token', token, {path: '/', sameSite: true})
+        throw redirect(303, '/butt/stories');
+
+      } else {
+        // TODO: I'd like to just display a message saying the login failed
+        // but we've gotta block an IP after a certain amount of failed logins... fail2ban?
+        console.log("login failed");
+      }
+
+    // close the db connection
+    } finally {
+      await client.close()
+    }
+  }
+}
---
+++ src/routes/butt/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+</script>
+
+<div class="horizontal">
+
+  <div class="vertical">
+    <h1>Hey, Cutie.</h1>
+    <h2>You know what to do.</h2>
+    <form class="login" method="POST">
+      <label for="username">Username:</label>
+      <input id="username" name="username" />
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      <button >Login</button>
+    </form>
+  </div>
+  
+  <img src="/fried_selfie_dark.png" alt="selfie of Lea Kapp, digitlized, pixelated, and saturated into four primary colors: whitish, blackish, purple, and magenta">
+</div>
+
+<style>
+  .horizontal {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+  .vertical {
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  h1 {
+    font-family: "Poiret One";
+    font-size: 96px;
+    margin: 0;
+  }
+  h2 {
+    font-family: "Poiret One";
+    font-size: 24px;
+    margin-top: 4px;
+    margin-bottom: 36px;
+  }
+  .login {
+    display: flex;
+    flex-direction: column;
+    background-color: var(--grayish);
+    border-radius: 24px;
+    padding: 24px;
+    font-size: 36px;
+  }
+  label {
+    font-family: "Poiret One";
+    margin-bottom: 12px;
+  }
+  input {
+    width: 360px;
+    margin-bottom: 24px;
+    font-size: 28px;
+  }
+  button {
+    background-color: var(--primary);
+    border: none;
+    color: var(--whitish);
+    font-family: "Poiret One";
+    border-radius: 16px;
+    padding: 12px 24px 12px 24px;
+    margin: 12px auto auto auto;
+  }
+  img {
+    max-height: 100vh;
+  }
+</style>
