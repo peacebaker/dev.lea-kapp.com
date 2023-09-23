87ef1336fd4fd09a90378264b48a1824069d2a88
peacebaker <116932680+peacebaker@users.noreply.github.com>
2023-09-23 15:18:37 -0400

    load stories into butt

--- src/routes/butt/stories/+page.server.js
+++ src/routes/butt/stories/+page.server.js
@@ -1,12 +1,12 @@
-// import {uri} from "$lib/config.js";
+import {uri, dbname} from "$lib/config.js";
 
 import {error} from "@sveltejs/kit";
-// import {MongoClient} from "mongodb";
+import {MongoClient} from "mongodb";
 
 export async function load({cookies}) {
 
   // connect to local MongoDB instance
-  // const client = new MongoClient(uri);
+  const client = new MongoClient(uri);
 
   // check current cookies
   const username = cookies.get('username');
@@ -17,6 +17,30 @@ export async function load({cookies}) {
     throw error(403, "Not logged in.");
   }
 
-  // 
-  
+  try {
+
+    // try to find the session in Mongo
+    const db = client.db(dbname);
+    const sessions = db.collection('sessions');
+    const query = { user: username, token: token };
+    const session = await sessions.findOne(query);
+
+    // TODO: implement some kinda session timeout
+    
+
+    // if no session is found, display error page.
+    if (!session) {
+      throw error(403, "Not logged in.");
+
+    // if the user is already logged in, load the stories
+    // for now, we can load all of them w/o causing too much strain
+    } else {
+      const collection = db.collection("stories");
+      const stories = await collection.find().toArray();
+      return {stories};
+    }
+
+  } finally {
+    await client.close();
+  }
 }
