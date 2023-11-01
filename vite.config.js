15bf034f854ee29a22dfa05318f664e15d1bcce7
peacebaker <116932680+peacebaker@users.noreply.github.com>
2023-10-31 21:23:09 -0400

    add self-hosted tinymce

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+    sveltekit(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/tinymce/*',
+          dest: 'tinymce'
+        }
+      ]
+    })
+  ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
