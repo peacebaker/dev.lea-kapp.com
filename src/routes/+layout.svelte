421cca97c1bdaccfeffc47579c32f2d33b2b8210
peacebaker <116932680+peacebaker@users.noreply.github.com>
2023-11-15 14:13:25 -0500

    add global link styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,4 +31,26 @@
     font-family: "Poiret One";
     font-size: 3rem;
   }
+  @keyframes link {
+    0% {
+      color: var(--whitish);
+    }
+    100% {
+      color: var(--primary);
+    }
+  }
+  :global(a:link, a:visited) {
+    color: var(--whitish);
+    text-decoration: none;
+  }
+  :global(a:hover) {
+    color: var(--primary);
+    text-decoration: none;
+    animation-name: link;
+    animation-duration: 1s;
+    animation-timing-function: ease;
+  }
+  :global(a:active) {
+    color: var(--secondary);
+  }
 </style>
