58454d648f1396d48949e99f91d8e5289d4da192
peacebaker <116932680+peacebaker@users.noreply.github.com>
2023-12-05 18:41:40 -0500

    flesh out the bio

--- src/routes/bio/+page.svelte
+++ src/routes/bio/+page.svelte
@@ -8,7 +8,25 @@
     <a href="/" id="bioLink">Menu</a>
     
     <div class="bio">
-      <p>I swear: I'm a real person, not an AI.  I might be trapped inside the net, but I'm no Alt Cunningham.</p>
+      <p>Hey there,</p>
+      <p>
+        My name is Lea Kapp.  I'm an developer, author, philosopher, foodie, and more recently, an artist.
+        This website is where I'll post or link to all of my content, a portal to my web presence.
+        It's like a social media page without the constant stream of incoming noise.
+      </p>
+      <p>
+        A little bit about me:
+      </p>
+      <ul>
+        <li>I'm a lover of fine wine and cheap beer.</li>
+        <li>I believe artificial intelligences should have personhood status.</li>
+        <li>My favorite DnD class is a bard.</li>
+        <li>I've solved most of life's ultimate questions, and I don't care to elaborate.</li>
+        <li>The next time I die, I want to come back as a butterfly.</li>
+      </ul>
+      <p>
+        All intellectual property rights reserved, except where otherwise noted.
+      </p>
     </div>
   </div>
 
@@ -50,5 +68,8 @@
   }
   img {
     max-height: 100vh;
+  }
+  p {
+    margin: .67rem .33rem;
   }
 </style>
